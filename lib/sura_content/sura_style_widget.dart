acdbcb354bf4b2117ca4d6d550e45e3f9cbe8bd7
Nagham Abdelrahman <naghamabdelrahman62@gmail.com>
2022-12-05 09:05:58 -1200

    add quran files update sura content screen to read sura files create sura style widget

---
+++ lib/sura_content/sura_style_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SuraStyleWidget extends StatelessWidget {
+  String content;
+  int index;
+
+  SuraStyleWidget(this.content, this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 10),
+      child: Text(
+        '$content[$index]',
+        textDirection: TextDirection.rtl,
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.05),
+      ),
+    );
+  }
+}
